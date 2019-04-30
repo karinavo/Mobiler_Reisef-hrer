6fb81a7b95a0d5858dd0b02d86086f3df93d2cb0
Ruslan Jelbuldin <a01407036@unet.univie.ac.at>
2019-04-30 17:11:43 +0200

    navigation added

---
+++ Source/app/src/main/java/at/ac/univie/reisefuehrer/MainActivity.java
@@ -0,0 +1,47 @@
+package at.ac.univie.reisefuehrer;
+
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.design.widget.BottomNavigationView;
+import android.support.v4.app.Fragment;
+import android.support.v7.app.AppCompatActivity;
+import android.view.MenuItem;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity implements BottomNavigationView.OnNavigationItemSelectedListener {
+
+    private TextView mTextMessage;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
+        BottomNavigationView navigation = findViewById(R.id.navigation);
+        navigation.setOnNavigationItemSelectedListener(this);
+        replaceFragment(new HomeFragment());
+    }
+
+    //changes current fragment to chosen fragment
+    public boolean replaceFragment(Fragment fragment) {
+        if(fragment != null){
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .replace(R.id.fragment_container, fragment)
+                    .commit();
+            return true;
+        }
+        return false;
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(@NonNull MenuItem menuItem) {
+        Fragment fragment = null;
+        switch (menuItem.getItemId()) {
+            case R.id.navigation_home:
+                fragment = new HomeFragment();
+                break;
+        }
+
+        return replaceFragment(fragment);    }
+}
