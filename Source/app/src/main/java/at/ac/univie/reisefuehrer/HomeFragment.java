715c4115e10f6b0210f672facda08b04f5eabce6
Karyna <57938@mail.ru>
2019-05-07 21:47:36 +0200

    Page

--- Source/app/src/main/java/at/ac/univie/reisefuehrer/HomeFragment.java
+++ Source/app/src/main/java/at/ac/univie/reisefuehrer/HomeFragment.java
@@ -13,6 +13,7 @@ import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.RelativeLayout;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,18 +22,18 @@ import de.hdodenhof.circleimageview.CircleImageView;
 import in.goodiebag.carouselpicker.CarouselPicker;
 
 public class HomeFragment extends Fragment {
-    LinearLayout category_museums;
+    RelativeLayout category_museums;
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_home_fragment,null);
 
         category_museums = view.findViewById(R.id.museums);
-        LayoutInflater inflater1 = LayoutInflater.from(this.getContext());
+        LayoutInflater inflater1 = LayoutInflater.from(this.getActivity());
         View view1 =inflater1.inflate(R.layout.cat_museums,category_museums,false);
         ImageView im = view1.findViewById(R.id.albertina);
         im.setImageResource(R.mipmap.ic_launcher);
-        category_museums.addView(im);
+        category_museums.addView(view1);
 
 
         return view;
