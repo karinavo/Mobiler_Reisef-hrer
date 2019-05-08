31397bbcf75c93810bdb14f36131838e61f3e331
Karyna <57938@mail.ru>
2019-05-08 15:11:10 +0200

    Added changes in home page

--- Source/app/src/main/java/univie/travelguide/HomeFragment.java
+++ Source/app/src/main/java/univie/travelguide/HomeFragment.java
@@ -11,26 +11,35 @@ import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
+import android.widget.ListView;
 import android.widget.RelativeLayout;
 import android.widget.ScrollView;
 
 import java.util.ArrayList;
 import java.util.List;
 
-public class HomeFragment extends Fragment {
-    LinearLayout category_museums;
+public class HomeFragment extends Fragment   {
+    LinearLayout linearLayout;
     ScrollView bigScrollview;
     RelativeLayout relativeLayout;
     List<ImageView> images_museums = new ArrayList<>();
+
     @Nullable
     @Override
     public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         View view = inflater.inflate(R.layout.activity_home_fragment,null);
-        category_museums = view.findViewById(R.id.museums);
+        bigScrollview = view.findViewById(R.id.bigscrollview);
+        linearLayout = view.findViewById(R.id.llayout);
         LayoutInflater inflater1 = LayoutInflater.from(getActivity().getApplicationContext());
         View view1 =inflater1.inflate(R.layout.cat_museums,category_museums,false);
+        ListView listView = new ListView(getActivity().getApplicationContext());
+        //to display each result within view:
+        final ArrayAdapter<ImageView> adapter = new ArrayAdapter<I>(view.getContext(), android.R.layout.simple_list_item_1,images_museums);
+
+        listView.setAdapter(adapter);
         //BILDER
         ImageView albertina = view1.findViewById(R.id.albertina);
         ImageView belvedere = view1.findViewById(R.id.belvedere);
@@ -49,12 +58,18 @@ public class HomeFragment extends Fragment {
         images_museums.add(mumok);
         images_museums.add(mak);
         images_museums.add(kunsthistm);
-       /* for(ImageView im : images_museums){
-            im.setImageResource(R.mipmap.ic_launcher);
-        }*/
-        //albertina.setImageResource(R.mipmap.ic_launcher);
-        //belvedere.setImageResource(R.mipmap.ic_launcher);
-        category_museums.addView(view1);
+
+        listView.addView(view1);
+        linearLayout.addView(listView);
+        bigScrollview.addView(linearLayout);
+        for(ImageView im : images_museums){
+            im.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+
+                }
+            });
+        }
 
         return view;
     }
